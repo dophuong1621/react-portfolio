954c1cdde1c54e326a5114509ffcf5064d6b1b36
JiRim <86226518+dophuong1621@users.noreply.github.com>
2026-04-21 15:07:15 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/react-portfolio/' : '/',
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          swiper: ['swiper'],
+          framer: ['framer-motion'],
+          particles: ['@tsparticles/react', '@tsparticles/slim', '@tsparticles/engine'],
+          tagcloud: ['TagCloud'],
+          icons: ['react-icons']
+        }
+      }
+    }
+  }
 })
