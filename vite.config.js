e853d0e868dd2f5d9e17a7baee33fc39455c59a6
JiRim <86226518+dophuong1621@users.noreply.github.com>
2026-04-21 15:53:42 +0700

    Update: Rename source

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/react-portfolio/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000,
